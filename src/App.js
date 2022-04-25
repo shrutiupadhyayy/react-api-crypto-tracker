6a5c30adb2acdce09b3652fa72c6720fd458666c
Shruti <shrutii.5599@gmail.com>
2022-04-25 14:59:09 +0530

    third

--- src/App.js
+++ src/App.js
@@ -45,8 +45,10 @@ function App() {
           name={coin.name} 
           image={coin.image} 
           symbol={coin.symbol}
-          volume={coin.market_cap}
+          marketcap={coin.market_cap}
           price={coin.current_price}
+          priceChange={coin.price_change_percentage_24h}
+          volume={coin.total_volume}
           />;
 
       })}
---
+++ src/installed axios and imported in app.js
@@ -0,0 +1,5 @@
+installed axios and imported in app.js
+using the hooks, usestate and useeffect
+
+writing the function for the actual api
+
